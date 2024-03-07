929cff6fed624c77261804ae8b31a00940252695
Juan <juanjosezapatagonzalez@hotmail.com>
2024-03-07 14:31:55 +0100

    Fix zoom

--- gym_multi_car_racing/multi_car_racing_bezier.py
+++ gym_multi_car_racing/multi_car_racing_bezier.py
@@ -80,7 +80,7 @@ SCALE       = 6.0        # Track scale (default = 6.0)
 TRACK_RAD   = 900/SCALE  # Track is heavily morphed circle with this radius (default = 900)
 PLAYFIELD   = 2000/SCALE # Game over boundary
 FPS         = 50        # Frames per second
-ZOOM        = 0.17        # Camera zoom (default = 2.7)
+ZOOM        = 2.7        # Camera zoom (default = 2.7)
 ZOOM_FOLLOW = True       # Set to False for fixed view (don't use zoom)
 
 
